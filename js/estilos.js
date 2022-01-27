390afa2a49227e004a73697ec050f29fdf7b122f
Denise Mayra Higa <deniiise.mayra@gmail.com>
2022-01-27 16:41:07 -0300

    Proyecto final

--- js/estilos.js
+++ js/estilos.js
@@ -60,6 +60,69 @@ let designList = [
     {id:"cupDe5",
         name:"Diseño de Star Wars",
         img: "../assets/tazas/diStarW1.jpg",
+    },
+    {id:"cupDe6",
+        name:"Diseño para Papá",
+        img: "../assets/tazas/diPapa1.jpg",
+    },
+    {id:"cupDe7",
+        name:"Diseño auto para Papá",
+        img: "../assets/tazas/diPapa2.jpg",
+    },
+    {id:"cupDe8",
+        name:"Diseño de Dragon Ball Z",
+        img: "../assets/tazas/diDragonBall1.jpg",
+    }
+]
+
+let tshirtsList = [
+    {id:"tshi1",
+        name: 'Musculosa diseño HazLoTuyo',
+        img:"../assets/indumentaria/musc1.1.png",
+        img2:"../assets/indumentaria/musc1.2.png",
+        price: 700,
+    },
+    {id:"tshi2",
+        name:"Musculosa diseño HazLoTuyo",
+        img:"../assets/indumentaria/musc2.1.png",
+        img2:"../assets/indumentaria/musc2.2.png",
+        price: 700,
+    },
+    {id:"tshi3",
+        name:"Remera Fluo diseño HazLoTuyo",
+        img:"../assets/indumentaria/remera1.1.png",
+        img2:"../assets/indumentaria/remera1.2.png",
+        price: 800,
+    },
+]
+
+let sweaterList =[
+    {id:"swea1",
+        name:"Buzo de Naruto",
+        img: "../assets/indumentaria/buzo2.1.png",
+        img2: "../assets/indumentaria/buzo2.2.png",
+        price: 1300,
+    },
+    {id:"swea2",
+        name:"Buzo Dibujo de nena",
+        img: "../assets/indumentaria/buzo1.1.png",
+        img2: "../assets/indumentaria/buzo1.2.png",
+        price: 1200,
+    }
+]
+
+let clothesDesignList = [
+    {id:"clothDe1",
+        name:"Diseño de Mujer Japonesa",
+        img: "../assets/indumentaria/diInduMujerJapon.jpg", 
+    },
+    {id:"clothDe2",
+        name:"Diseño de Samurai",
+        img: "../assets/indumentaria/diInduSamurai1.jpg", 
+    },
+    {id:"clothDe3",
+        name:"Diseño de Samurai con frase",
+        img: "../assets/indumentaria/diInduSamurai2.jpg", 
     }
 ]
 
@@ -166,6 +229,110 @@ for(let product of designList){
     `)
 }
 
+for(let product of tshirtsList){
+    
+    $("#prod_tshirts").append(`
+        <div class="card item__description" style="width: 18rem;">
+
+            <div id="${product.id}" class="carousel slide" data-bs-touch="false" data-bs-interval="false">
+                <div class="carousel-inner">
+                    <div class="carousel-item active">
+                        <img src="${product.img}" class="d-block w-100" alt="Imagen del Producto">
+                    </div>
+                    <div class="carousel-item">
+                        <img src="${product.img2}" class="d-block w-100" alt="Imagen del Producto">
+                    </div>
+                </div>
+                <button class="carousel-control-prev" type="button" data-bs-target="#${product.id}" data-bs-slide="prev">
+                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                    <span class="visually-hidden">Previous</span>
+                </button>
+                <button class="carousel-control-next" type="button" data-bs-target="#${product.id}" data-bs-slide="next">
+                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                    <span class="visually-hidden">Next</span>
+                </button>
+            </div>
+    
+            <div class="card-body">
+                <h5 class="card-title">${product.name}</h5>
+                <select name="" >
+                    <option value="">Seleccione un talle</option>
+                    <option value="s">S</option>
+                    <option value="m">M</option>
+                    <option value="l">L</option>
+                </select>
+                <p class="card-text">$ ${product.price}</p>
+                <a href="#" class="btn btn-primary addToCart">Agregar al Carrito</a>
+            </div>
+        </div>
+    `)
+}
+
+for(let product of sweaterList){
+    
+    $("#prod_sweater").append(`
+        <div class="card item__description" style="width: 18rem;">
+
+            <div id="${product.id}" class="carousel slide" data-bs-touch="false" data-bs-interval="false">
+                <div class="carousel-inner">
+                    <div class="carousel-item active">
+                        <img src="${product.img}" class="d-block w-100" alt="Imagen del Producto">
+                    </div>
+                    <div class="carousel-item">
+                        <img src="${product.img2}" class="d-block w-100" alt="Imagen del Producto">
+                    </div>
+                </div>
+                <button class="carousel-control-prev" type="button" data-bs-target="#${product.id}" data-bs-slide="prev">
+                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                    <span class="visually-hidden">Previous</span>
+                </button>
+                <button class="carousel-control-next" type="button" data-bs-target="#${product.id}" data-bs-slide="next">
+                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                    <span class="visually-hidden">Next</span>
+                </button>
+            </div>
+    
+            <div class="card-body">
+                <h5 class="card-title">${product.name}</h5>
+                <select name="" >
+                    <option value="">Seleccione un Talle</option>
+                    <option value="s">S</option>
+                    <option value="m">M</option>
+                    <option value="l">L</option>
+                </select>   
+                <p class="card-text">$ ${product.price}</p>
+                <a href="#" class="btn btn-primary addToCart">Agregar al Carrito</a>
+            </div>
+        </div>
+    `)
+}
+
+for(let product of clothesDesignList){
+    $("#prod_clothDesign").append(`
+        <div class="card item__description" style="width: 25rem;">
+            <img src="${product.img}" class="card-img-top" alt="Imagen del Producto">
+            <div class="card-body">
+                <h5 class="card-title">${product.name}</h5>
+                <select name="" class="selectBoton3">
+                    <option value="">Seleccione Prenda</option>
+                    <option value="Musculosa">Musculosa</option>
+                    <option value="Remera">Remera</option>
+                    <option value="Buzo">Buzo</option>
+                </select>
+                <select name="" >
+                    <option value="">Seleccione un Talle</option>
+                    <option value="s">S</option>
+                    <option value="m">M</option>
+                    <option value="l">L</option>
+                </select> 
+                <p class="card-text item-price3"></p>
+                <a href="#" class="btn btn-primary addToCart">Agregar al Carrito</a>
+            </div>
+        </div>
+
+    `)
+}
+
 $(".selectBoton").change(function(e){
     let child = e.target;
     let father = child.parentNode;
@@ -208,6 +375,23 @@ $(".selectBoton2").change(function(e){
     }
 })
 
+$(".selectBoton3").change(function(e){
+    let child = e.target;
+    let father = child.parentNode;
+
+    let message = father.querySelector(".item-price3");
+    
+    if(e.target.value == "Musculosa"){
+        message.innerHTML = `$ 700`;
+    }
+    else if(e.target.value == "Remera"){
+        message.innerHTML = `$ 800`;
+    }
+    else if(e.target.value == "Buzo"){
+        message.innerHTML = `$ 1200`;
+    }
+})
+
 // ----------------------- TOGGLE DEL CARRITO ------------------
 $("#cartImg").on("click", function(){
     $("#cartInfo").slideToggle();
@@ -232,15 +416,22 @@ function addToCart(e){
     let itemPrice = father.querySelector("p").textContent;
     let itemImg = father.querySelector("img").src;
 
-    let productinCart = {
-        nombre:itemTitle,
-        material:itemMaterial,
-        price:itemPrice,
-        img: itemImg,
-        cantidad:1
+    if(itemMaterial === ""){
+        alert("Por favor seleccione una opción")
+    }
+    else{
+        let productinCart = {
+            nombre:itemTitle,
+            material:itemMaterial,
+            price:itemPrice,
+            img: itemImg,
+            cantidad:1
+        }
+
+        addItemCart(productinCart);
     }
 
-    addItemCart(productinCart);
+    
 }
 
 // si ya seleccione el prod anteriormente solo lo suma
@@ -313,7 +504,6 @@ function removeItem(e){
     for(let i=0; i<cart.length; i++){
         if(cart[i].img === img){
             cart.splice(i, 1)
-            console.log("se elimina?")
         }
     }
 
@@ -381,3 +571,26 @@ $("#back2").on("click", function(){
     $("#progress").css("width", "240px");
 })
 
+// ------------------------ DESAFIO AJAX EN TU PROYECTO ---------------------   
+/* 
+let ubication = navigator.geolocation.getCurrentPosition(showUbi)
+function showUbi(data){
+    console.log(data);
+    let latitude = data.coords.latitude;
+    let longitude = data.coords.longitude;
+
+    let weatherapi = `http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=6ac6addf5d33fe1c34b2b23b3d39f7bb
+    `
+    $.get(weatherapi, function(datos){
+        console.log(datos)
+        let min = parseInt(datos.main.temp_min)-273.15;
+        let max = parseInt(datos.main.temp_max)-273.15;
+
+        let writeMin = min.toFixed(1);
+        let writeMax = max.toFixed(1);
+
+        let weatherContainer = document.getElementById("weather");
+        weatherContainer.innerHTML = `<h4>El clima en ${datos.name}</h4>
+                                    <p>La temperatura varia entre: ${writeMin}C - ${writeMax}C`
+    })
+} */
